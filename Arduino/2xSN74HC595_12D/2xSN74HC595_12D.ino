3333c45a828cf6fc68c98d5ce6bbcc22d84ecc68
03Filip0001 <50228608+03Filip0001@users.noreply.github.com>
2019-10-29 18:14:26 +0100

    Add files via upload

--- Arduino/2xSN74HC595_12D/2xSN74HC595_12D.ino
+++ Arduino/2xSN74HC595_12D/2xSN74HC595_12D.ino
@@ -1,8 +1,8 @@
 #include <HC595.h>
 
 const int chipCount = 2;
-const int clockPin = 2;
-const int latchPin = 3;
+const int clockPin = 3;
+const int latchPin = 2;
 const int dataPin = 4;
 
 HC595 ledArray(chipCount, latchPin, clockPin, dataPin);
@@ -17,10 +17,19 @@ void setup() {
 void loop() {
   for (int i = 0; i <= 4095; i++)
   {
-    for(int j = 0; j < 16;j++){
-          if (i & (0 << j)) ledArray.setPin(j,1);
-          else ledArray.setPin(j,1);
-}
-delay(500);
-}
+    for (int j = 0; j <= lastPin; j++) {
+      if (i & (1 << j)) ledArray.setPin(j, 1);
+      else ledArray.setPin(j, 0);
+    }
+    delay(25);
+  }
+
+  for (int i = 4095; i > 0; i--)
+  {
+    for (int j = 0; j <= lastPin; j++) {
+      if (i & (1 << j)) ledArray.setPin(j, 1);
+      else ledArray.setPin(j, 0);
+    }
+    delay(25);
+  }
 }
