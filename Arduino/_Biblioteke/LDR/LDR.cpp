b11a30712b401493d6197f0e94cd161a590c713e
03Filip0001 <50228608+03Filip0001@users.noreply.github.com>
2020-02-09 18:24:57 +0100

    Ispravka
    Jos ne radi inicijalizacija pinova

--- Arduino/_Biblioteke/LDR/LDR.cpp
+++ Arduino/_Biblioteke/LDR/LDR.cpp
@@ -2,15 +2,25 @@
 #include "Arduino.h"
 
 LDR::LDR(int SEG_A, int SEG_B, int SEG_C, int SEG_D, 
-		 int SEG_E, int SEG_F, int SEG_G,
-		 int base1, int base2, int base3, int base4){
+		 int SEG_E, int SEG_F, int SEG_G/*,
+		 int base1, int base2, int base3, int base4*/){
 	pin_A = SEG_A;
 	pin_B = SEG_B;
 	pin_C = SEG_C;
 	pin_D = SEG_D;
 	pin_E = SEG_E;
 	pin_F = SEG_F;
-	pin_G = SEG_G;
+	pin_G = SEG_G; 
+	/*
+	//OVO TI TREBA
+	pins[0] = SEG_A;
+	pins[1] = SEG_B;
+	pins[2] = SEG_C;
+	pins[3] = SEG_D;
+	pins[4] = SEG_E;
+	pins[5] = SEG_F;
+	pins[6] = SEG_G;
+	*/
 }
 
 void LDR::Setup(){
@@ -21,11 +31,17 @@ void LDR::Setup(){
 	pinMode(pin_E, OUTPUT);
 	pinMode(pin_F, OUTPUT);
 	pinMode(pin_G, OUTPUT);
-	
+	/*
 	pinMode(base1, OUTPUT);
 	pinMode(base2, OUTPUT);
 	pinMode(base3, OUTPUT);
 	pinMode(base4, OUTPUT);
+	
+	digitalWrite(base[0], HIGH);
+	digitalWrite(base[1], LOW);
+	digitalWrite(base[2], HIGH);
+	digitalWrite(base[3], LOW);
+	*/
 }
 
 void LDR::showNumber(int data){
@@ -36,13 +52,13 @@ void LDR::showNumber(int data){
 
 // Ovo ispod nije nista zavrseno
 // ne obaziri se na to
-/*
-void LDR::showInvNumber(uint16_t invData) {
-	for(int i = 6; i < 8; i++){
-			digitalWrite(pins[i], numbers[data][1] & (1 << i));
+
+void LDR::showInvNumber(int invData) {
+	for(int i = 0; i < 7; i++){
+			digitalWrite(pins[i], inv_numbers[invData] & (1 << i));
 	}
 }
-
+/*
 void LDR::InvertCurrent(int base1, int base2, int base3,int base4){
 	
 }*/
--- Arduino/_Biblioteke/LDR/LDR.h
+++ Arduino/_Biblioteke/LDR/LDR.h
@@ -8,48 +8,48 @@ class LDR{
 	private:
 		int pin_A,pin_B,pin_C,pin_D,pin_E,pin_F,pin_G;
 		int base1, base2, base3, base4;
-		int pins[7] = {pin_A,pin_B,pin_C,pin_D,pin_E,pin_F,pin_G};
-		//int pins[7] = {2,3,4,5,6,7,8};
+		//int pins[7] = {pin_A,pin_B,pin_C,pin_D,pin_E,pin_F,pin_G};
+		int pins[7] = {2,3,4,5,6,7,8};
+		//OVO TI TREBA
+		//int pins[7];
+		//int base[4] = {9,10,11,12};
 		int numbers[10] = {
-			// Nisam jos invertovao redosled
-			// Jesam
-			// Brojevi nisu dobro ispisani (osim 1)
-			129, //0b01111110 +
-			121, //0b01111001 + 
-			109, //0b01101101 +
-			134, //0b01111001 +
-			204, //0b00110011 +
-			164, //0b01011011 +
-			160, //0b01011111 +
-			143, //0b01110000 +
+			192, //0b00111111 + 00111111 11000000
+			121, //0b01111001 + 01111001 10000110
+			164, //0b01101101 + 01011011 10100100
+			176, //0b01111001 + 01001111 10110000
+			153, //0b00110011 + 01100110 10011001
+			146, //0b01011011 + 01101101 10010010
+			130, //0b01011111 + 01111101 10000010
+			143, //0b01110000 + 
 			128, //0b01111111 +
 			132  //0b01111011 +
 		};
 		
 		// Ne obaziri se na ovo
-		int inv_numbers[10][2] = {
+		int inv_numbers[10] = {
 			// Nisam jos invertovao redosled
-			{0, 0b00000001},
-			{1, 0b01001111},
-			{2, 0b00010010},
-			{3, 0b00000110},
-			{4, 0b01001100},
-			{5, 0b00100100},
-			{6, 0b00100000},
-			{7, 0b00001111},
-			{8, 0b00000000},
-			{9, 0b00000100}
+			63,		 	//11000000 00111111
+			134, //10000110 01111001
+			0b00010010,
+			0b00000110,
+			0b01001100,
+			0b00100100,
+			0b00100000,
+			0b00001111,
+			0b00000000,
+			0b00000100
 		};
 	public:
 		LDR(int SEG_A, int SEG_B, int SEG_C, int SEG_D, 
-			int SEG_E, int SEG_F, int SEG_G,
-			int base1, int base2, int base3, int base4);
+			int SEG_E, int SEG_F, int SEG_G/*,
+			int base1, int base2, int base3, int base4*/);
 		void Setup();
 		void showNumber(int data);
 		
 		// Ovo ispod nije nista zavrseno
 		// ne obaziri se na to
-		//void showInvNumber(uint16_t invData);
+		void showInvNumber(int invData);
 		//void InvertCurrent(int base1, int base2, int base3,int base4);
 			
 
